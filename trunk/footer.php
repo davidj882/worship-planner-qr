b6198a2d3728035a8e5a022f1e6206d3091301db
worship.planner <worship.planner@95de7173-9139-0410-81d6-a55f48108760>
2008-03-02 23:08:40 +0000

    Changed hyperlink to google code site

--- trunk/footer.php
+++ trunk/footer.php
@@ -8,7 +8,7 @@ if($_REQUEST['printable'] == "yes"){
 	echo("\n</body></html>");
 }else{
 echo("<br><small><i><a href='".$PHP_SELF."?".$QUERY_STRING."&printable=yes'>View Printer-Friendly Version</a></i></small><br>");
-echo("<br><br><small><small>Powered by <a href='http://www.worshipplanner.net'>Worship Planner QR</a> 1.1\n");
+echo("<br><br><small><small>Powered by <a href='http://code.google.com/p/worship-planner-qr/'>Worship Planner QR</a> 1.1\n");
 
 	if($myWP->userIsAdmin())
 	{
